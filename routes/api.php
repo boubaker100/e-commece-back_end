6df9d94d11225e39b80d5013e6788ea7333e5a26
boubaker100 <bakerbarca100@gmail.com>
2025-06-22 18:41:45 +0100

    add DB test route

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,15 @@ use App\Http\Controllers\UsersContoller;
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['message' => '✅ Connected to database!']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => '❌ DB Error: ' . $e->getMessage()]);
+    }
+});
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
