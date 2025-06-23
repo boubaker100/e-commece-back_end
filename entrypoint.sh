94b0d4c756ce43fd6fe5fcf77bd0c1de27a77770
boubaker100 <bakerbarca100@gmail.com>
2025-06-23 19:25:14 +0100

    fix: remove invalid flag from php artisan serve

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /var/www
 
-# فقط تشغيل السيرفر، بدون migrations ولا seeding
-php artisan serve --host=0.0.0.0 --port=8000 --no-interaction
+# شغل الخادم فقط
+php artisan serve --host=0.0.0.0 --port=8000
