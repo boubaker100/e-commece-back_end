90712041357e243d335e043f6513516e21e9125f
boubaker100 <bakerbarca100@gmail.com>
2025-06-25 00:44:55 +0100

    Restore original product titles using seeder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+#
 php artisan config:clear
+#
+
 php artisan migrate --force
-php artisan passport:install --force
+#php artisan passport:install --force
 php artisan db:seed --force
 exec "$@"
 
