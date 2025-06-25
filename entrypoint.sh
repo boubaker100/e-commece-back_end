aa1a3e5f1938262342c3782d09d30c4b9d90326b
boubaker100 <bakerbarca100@gmail.com>
2025-06-25 11:50:35 +0100

    Restore original product  using seeder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-#
+# تنظيف الكاش
 php artisan config:clear
-#
 
+# حذف كل الجداول وإنشاءها من جديد
 php artisan migrate:fresh --force
+
+# تثبيت Passport (ينشئ المفاتيح وجداول OAuth)
 php artisan passport:install --force
 
+# ملء قاعدة البيانات بالسيدرز
 php artisan db:seed --force
 
-
-
+# تشغيل السيرفر أو العملية المطلوبة
 exec "$@"
-
