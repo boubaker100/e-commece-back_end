7ff5dad87fe619950b9fb83083f6d98dffe2d3e5
boubaker100 <bakerbarca100@gmail.com>
2025-06-25 17:53:21 +0100

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,5 @@
 
 php artisan config:clear
 php artisan migrate:fresh --force
-php artisan passport:install --force
-php artisan db:seed --force
-
+  
 exec php artisan serve --host=0.0.0.0 --port=8000
