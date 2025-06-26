da930caaaedf999c2a7d97945e11e33593ddacbf
boubaker100 <bakerbarca100@gmail.com>
2025-06-27 00:42:27 +0100

    fix: update CORS config to allow frontend

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','https://e-commerce-gamma-liart.vercel.app/'],
+    'allowed_origins' => ['https://e-commerce-gamma-liart.vercel.app'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
