cf675679568e79a7e29420bc2e8faadf82028c1a
boubaker100 <bakerbarca100@gmail.com>
2025-06-27 19:23:29 +0100

    تحديث منطق الدور وتفعيل مفتاح

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => $isAdmin ? 1995 : 2001,
+            'role' => $request->role,
         ]);
         $token = $user->createToken('token')->accessToken;
         $refreshToken = $user->createToken('authTokenRefresh')->accessToken;
